1f02294f9de52cfcd92426d0ab6e7925f8357d6b
Manuel <manuel.mayo.lado@gmail.com>
2016-05-27 05:48:43 +0200

    update

--- ipv4.py
+++ ipv4.py
@@ -59,7 +59,7 @@ def red(ip,mask):
 			red += "1"
 		else:
 			red += "0"
-	return bin2dec(red)
+	return bin2ip(red)
 	
 #BROADCAST DE RED CON DATOS EN FORMATO DECIMAL PUNTEADO
 def broadcast_bin(ip_bin,mask_bin):
@@ -83,32 +83,143 @@ def broadcast(ip,mask):
 			broadcast += r
 		else:
 			broadcast += "1"
-	return bin2dec(broadcast)
+	return bin2ip(broadcast)
 
+#COMPARACIÓN DA MÁSCARA DE RED COAS ANTIGUAS CLASES
+def clase(mask):
+	if mask == "255.0.0.0":
+		return "A"
+	elif mask == "255.255.0.0":
+		return "B"
+	elif mask == "255.255.255.0":
+		return "C"
+	else:
+		return "None"
+
+#ÁMBITO IP (PÚBLICA OU PRIVADA)
+def ambito(ip):
+	if red(ip,"255.0.0.0") == "10.0.0.0":
+		return "Direccion Privada"
+	elif red(ip,"255.240.0.0") == "172.16.0.0":
+		return "Direccion Privada"
+	elif red(ip,"255.255.0.0") == "192.168.0.0":
+		return "Direccion Privada"
+	elif ip[:7] == "169.154":
+		return "APIPA (Automatic Private IP Addressing)"
+	elif ip == "255.255.255.255":
+		return "Broadcast limitado (difusion)"
+	elif ip[:3].isdigit() and int(ip[:3]) >= 224:
+		return "Direccion reservada para Investigación"
+	elif ip == "0.0.0.0":
+		return "Ruta predeterminada"
+	elif ip[:3] == "127":
+		return "Bucle Local"
+	else:
+		return "Direccion Publica"
+		
+#TIPO DE DIRECCIÓN (HOST, RED, BROADCAST, ETC)
+def tipo(ip,mask):
+	if mask == "255.255.255.255":
+		return "Host"
+	elif ip == "0.0.0.0":
+		return "Ruta predeterminada"
+	elif ip[:3] == "127":
+		return "Bucle Local"
+	elif ip == "255.255.255.255":
+		return "Broadcast limitado (difusion)"
+	elif ip[:3].isdigit() and int(ip[:3]) >= 224:
+		return "Direccion reservada para Investigación"
+	elif ip[:7] == "169.154":
+		return "APIPA (Automatic Private IP Addressing)"
+	elif ip == red(ip,mask):
+		return "Direccion de Red"
+	else:
+		return "Host"
+
+#NÚMERO DE SUBREDES TEÓRICAS
+def n_subr_teoricas(bin_mask):
+	bit_1 = 0
+	for bit in bin_mask:
+		if bit == "1":
+			bit_1 += 1
+	return "2^"+str(bit_1)+" = "+str(2 ** bit_1)
+	
+#NÚMERO DE HOSTS TEÓRICOS
+def n_hosts_teoricos(bin_mask):
+	bit_0 = 0
+	bit_1 = 0
+	for bit in bin_mask:
+		if bit == "0":
+			bit_0 += 1
+		else:
+			bit_1 += 1
+	return ["2^"+str(bit_0)+" = "+str(2 ** bit_0),
+			"Total: "+str((2 ** bit_1) * 2 ** bit_0)]
+			
+#COMMUTACIÓNS DE BITS
+def comm(n):
+	t_bits = 2**n
+	list_bits = []
+	valor = 0
+	for x in range(t_bits):
+		valor_bin = dec2bin(valor)
+		valor_bin = ("0"*(n-len(str(valor_bin))))+str(valor_bin)
+		list_bits.append(valor_bin)
+		valor += 1
+	return list_bits
+
+#DICCIONARIO DE SUBREDES
+def subredes(red,mask,bits):
+	bit_1 = 0
+	dict_subr = {}
+	for bit in mask:
+		if bit == "1":
+			bit_1 += 1
+	bits = min(bits,32)
+	bits = max(bits,bit_1)
+	bytes_subred = bits-bit_1
+	n_subredes = 2**bytes_subred
+	n_hosts_x_subred = 2**(32-bits)
+	mask_subred = ("1"*bits)+("0"*(bits-bit_1))
+	bits_subredes = comm(bytes_subred)
+	print "Numero de Subredes:\t"+str(len(bits_subredes))
+	print "Hosts por Subred:\t"+str((2**(32-bits))-2)
+	print "Mascara:\t\t"+bin2ip(mask_subred)+"\t"+mask_subred
+	for sub in bits_subredes:
+		subred = red[:bit_1]+sub+red[bit_1+bytes_subred:]
+		print subred+"\t"+bin2ip(subred)+"/"+str(bits)
+	
+	
+#CLASE
 class rede():
 	def __init__(self,ip,mask="255.255.255.255"):
 		self.ip = [ip,ip2bin(ip)]
-		self.mask = [mask,ip2bin(mask)]
+		self.mask = [mask,ip2bin(mask),
+					"/"+str(len([x for x in ip2bin(mask) if x=="1"]))]
 		red_b = red_bin(self.ip[1],self.mask[1])
 		self.red = [bin2ip(red_b),red_b]
 		broadcast_b = broadcast_bin(self.ip[1],self.mask[1])
 		self.broadcast = [bin2ip(broadcast_b),broadcast_b]
-		if self.mask[0] == "255.0.0.0":
-			self.clase = "A"
-		elif self.mask[0] == "255.255.0.0":
-			self.clase = "B"
-		elif self.mask[0] == "255.255.255.0":
-			self.clase = "C"
-		else:
-			self.clase = None
-		self.all = [self.ip,self.mask,self.red,self.broadcast,self.clase]
+		self.clase = clase(self.mask[0])
+		self.ambito = ambito(self.ip[0])
+		self.tipo = tipo(self.ip[0],self.mask[0])
+		self.n_subrs_teoricas = n_subr_teoricas(self.mask[1])
+		self.n_hosts_teorcios = n_hosts_teoricos(self.mask[1])
+		self.all = [self.ip,self.mask,self.red,self.broadcast,
+					self.clase,self.ambito,self.tipo,
+					self.n_subrs_teoricas,self.n_hosts_teorcios]
 	def __repr__(self):
 		string_repr = ""
-		for name,info in zip(["Address","Netmask","Network","Broadcast","Clase"],self.all):
+		for name,info in zip(["Address:","Netmask:","Network:","Broadcast:",
+							"Clase:\t","Ambito:\t", "Tipo:\t","Numero Redes Teoricas:",
+							"Numero Hosts Teoricos:"],
+							self.all):
 			if type(info).__name__=="list":
-				string_repr += name+":\t"+info[0]+"\t"+info[1]+"\n"
+				string_repr += name+"\t"+"\t".join([str(x) for x in info])+"\n"
 			else:
-				string_repr += name+":\t\t"+info+"\n"
+				string_repr += name+"\t"+str(info)+"\n"
 		return string_repr
+	def subredes(self,bits):
+		return subredes(self.red[1],self.mask[1],bits)
 		
 		
